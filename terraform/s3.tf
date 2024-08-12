8c5cd643407dea63fd916b2782cf6cd93f87de30
Adam Maas <adamjm1220@gmail.com>
2024-08-12 17:43:21 -0400

    initial infra commit

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "unique-bucket-74739037450973402752947054" {
-  bucket = "unique-bucket-74739037450973402752947054"
+# resource "aws_s3_bucket" "unique-bucket-74739037450973402752947054" {
+#   bucket = "unique-bucket-74739037450973402752947054"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
