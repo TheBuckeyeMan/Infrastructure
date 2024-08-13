a8d6c9c9e4a546b9410eca7a0edbffdc0b7ed3f1
Adam Maas <adamjm1220@gmail.com>
2024-08-13 18:01:57 -0400

    Deploy ECR

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,32 @@
+# Declare the ECS Cluster
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "ecs_cluster"
+}
+
+# Declare the ECS Task Definition
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                = "ecs_task"
+  container_definitions = jsonencode([
+    {
+      name  = "placeholder-container"
+      image = "hello-world-placeholder"
+      cpu   = 256
+      memory = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "512"
+  cpu                      = "256"
+}
+
 resource "aws_ecs_service" "ecs_service" {
     name = "ecs_service"
     cluster = aws_ecs_cluster.ecs_cluster.id
