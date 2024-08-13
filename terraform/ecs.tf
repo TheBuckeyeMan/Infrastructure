a8120a0165eb3333451326b463c0ba9884672887
Adam Maas <adamjm1220@gmail.com>
2024-08-13 18:03:42 -0400

    Deploy ECR

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -30,7 +30,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
 resource "aws_ecs_service" "ecs_service" {
     name = "ecs_service"
     cluster = aws_ecs_cluster.ecs_cluster.id
-    task_definition = aws_ecs_task_definition.ecs_tasl.arn
+    task_definition = aws_ecs_task_definition.ecs_task.arn
     desired_count = 1
     launch_type = "FARGATE"
     network_configuration {
