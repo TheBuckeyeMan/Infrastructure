098e45cf95c44a8bf632ca2fbc55e2cf3ecac3a2
Adam Maas <adamjm1220@gmail.com>
2024-08-13 17:55:36 -0400

    Deploy ECR

--- terraform/secruity_groups.tf
+++ terraform/secruity_groups.tf
@@ -1,21 +1,23 @@
 resource "aws_security_group" "security_group" {
-    name = "http_security_group"
-    description = "ALLOW ATTP Traffic"
-    vpc_id = aws_vpc.vpc.id
+  name        = "http_security_group"
+  description = "ALLOW HTTP Traffic"
+  vpc_id      = aws_vpc.vpc.id
 
-    ingress = {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_block = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name = "http_security_group"
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http_security_group"
+  }
 }
